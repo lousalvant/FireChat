f3cb717e442fe57bfdbe57dd00dbd911b0a189bc
lousalvant <lousalvant@gmail.com>
2024-10-18 06:47:20 -0400

    first commit

---
+++ FireChat/FireChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireChatApp.swift
+//  FireChat
+//
+//  Created by Lou-Michael Salvant on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct FireChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
