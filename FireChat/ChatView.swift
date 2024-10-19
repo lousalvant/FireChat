bc58b0172ed4bfd5a6ba79dc02d987176fb8687a
lousalvant <lousalvant@gmail.com>
2024-10-19 12:03:17 -0400

    Step 10: Create UI for sending a message

--- FireChat/ChatView.swift
+++ FireChat/ChatView.swift
@@ -27,6 +27,12 @@ struct ChatView: View {
                         }
                     }
                 }
+                .safeAreaInset(edge: .bottom) { // <-- Add safeAreaInset modifier to add and display send message view above the bottom safe area
+                    SendMessageView { messageText in // <-- Add SendMessageView
+                        // TODO: Save message to Firestore
+                        
+                    }
+                }
                 .defaultScrollAnchor(.bottom)
                 .navigationTitle("Chat") // Title for the chat
                 .navigationBarTitleDisplayMode(.inline)
@@ -40,6 +46,39 @@ struct ChatView: View {
             }
         }
     }
+
+struct SendMessageView: View {
+    var onSend: (String) -> Void // <-- Closure called with a message passed in when send message button is tapped
+
+    @State private var messageText: String = "" // <-- Local state managed var to hold the message text as user types
+
+    var body: some View {
+        HStack(alignment: .bottom, spacing: 0) {
+            TextField("Message", text: $messageText, axis: .vertical) // <-- Message text field
+                .padding(.leading)
+                .padding(.trailing, 4)
+                .padding(.vertical, 8)
+
+            // Send message button
+            Button {
+                onSend(messageText) // <-- Call onSend closure passing in the message text when send button is tapped
+                messageText = "" // <-- Clear the message text after being sent
+            } label: {
+                Image(systemName: "arrow.up.circle.fill") // <-- Use arrow image from SFSymbols
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .bold()
+                    .padding(4)
+            }
+            .disabled(messageText.isEmpty) // <-- Disable button if text is empty
+        }
+        .overlay(RoundedRectangle(cornerRadius: 19).stroke(Color(uiColor: .systemGray2)))
+        .padding(.horizontal)
+        .padding(.vertical, 8)
+        .background(.thickMaterial) // <-- Add material to background
+        //        .focused($isMessageFieldFocused)
+    }
+}
 
 struct MessageRow: View {
     let text: String // <-- The message text
